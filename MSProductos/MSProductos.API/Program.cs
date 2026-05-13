4c34c1e62d5cca9c583ab1252ed545b0732c3f9b
Joshx567 <jose.lopez.s@ucb.edu.bo>
2026-05-13 17:23:41 -0400

    chore: limpiar archivos temporales

--- MSProductos/MSProductos.API/Program.cs
+++ MSProductos/MSProductos.API/Program.cs
@@ -1,18 +1,23 @@
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
+// Controllers
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+
+// Swagger
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+// Swagger solo en desarrollo
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
+
+// HTTPS
+app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
