da4f13750c80aa1a7d76607573ee57dac6c7df53
RashLop <lopezmejiarasshid@outlook.com>
2026-05-26 09:02:23 -0400

    fix: pagina de detalles de cuenta y seguridad mejorados, para el usuario

--- Frontend/FrontendVCare/Pages/Account/Seguridad.cshtml.cs
+++ Frontend/FrontendVCare/Pages/Account/Seguridad.cshtml.cs
@@ -24,7 +24,7 @@ namespace FrontendVCare.Pages.Account
                 
                 if (TempData["PasswordError"] != null)
                 {
-                    PasswordError = TempData["PasswordError"].ToString();
+                    PasswordError = TempData["PasswordError"]?.ToString();
                 }
             }
             catch (Exception ex)
